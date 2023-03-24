86b3f3a08492aa911bf08b72029987b917113693
florence wangechi <florencewangechim@gmail.com>
2023-03-24 21:10:12 +0300

    Complete Assingment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const mainElement = document.getElementById("main");
+if(mainElement){
+    mainElement.remove();
+}
+
+const  newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+
+
+const name = "Florence Wangechi";
+newHeader.textContent = "${name} is the champion";
+ 
+document.body.appendChild(newHeader);
